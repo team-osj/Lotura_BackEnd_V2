014b2864c2aad92187e4bf1874c9c083eafbbada
velyn666 <ni1722650@gmail.com>
2024-11-18 19:33:29 +0900

    add web Socket adapter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useWebSocketAdapter(new WsAdapter(app));
   app.enableCors();
 
   await app.listen(3000);
