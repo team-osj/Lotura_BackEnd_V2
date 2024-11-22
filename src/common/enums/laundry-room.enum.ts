89cf54b8a733e519f5efc91138ca98e653027334
O-damnpr <ni1722650@gmail.com>
2024-11-23 00:45:35 +0900

    세탁실 enum 추가, 각 컬럼 설명 주석 추가

---
+++ src/common/enums/laundry-room.enum.ts
@@ -0,0 +1,5 @@
+export enum LaundryRoomType {
+  MENS_FIRST = 'MENS_FIRST',
+  MENS_SECOND = 'MENS_SECOND',
+  WOMENS = 'WOMENS',
+}
--- src/entities/device.entity.ts
+++ src/entities/device.entity.ts
@@ -1,25 +1,46 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { LaundryRoomType } from '../common/enums/laundry-room.enum';
 
 @Entity('device')
 export class Device {
   @PrimaryGeneratedColumn()
   id: number;
+  // 기기 고유 아이디
 
+  // 화면에 표시되는 기기의 아이디로 W1-1 - 1번세탁기, D1-1 - 1번 건조기 라고 생각하면 됩니다.
   @Column()
   view_id: string;
 
+  // 기기 현재 상태
+  // 0: 작동중
+  // 1: 사용 가능
+  // 2: 연결 끊김
+  // 3: 고장나씀
   @Column()
   state: number;
 
+  //디바이스의 직전 상태로 상태 변경 추적을 위해 저장해둔겁니다.
   @Column()
   prev_state: number;
 
+  // 디바이스 종류
+  // WASH: 세탁기
+  // DRY: 건조기
   @Column()
   device_type: string;
 
+  // 어느 세탁실?
+  @Column({
+    type: 'enum',
+    enum: LaundryRoomType,
+  })
+  room_type: LaundryRoomType;
+
+  // state가 0으로 변경된 시간 즉 켜진시간
   @Column({ type: 'datetime', nullable: true })
   ON_time: Date;
 
+  // state가 1로 변경된 시간 즉 꺼진시간
   @Column({ type: 'datetime', nullable: true })
   OFF_time: Date;
 }
