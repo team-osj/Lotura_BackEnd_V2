3d62d3820fbfc486121994c9276c49dcf5386114
velyn666 <ni1722650@gmail.com>
2024-11-24 00:53:04 +0900

    device module - add device controller

--- src/device/device.module.ts
+++ src/device/device.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Device } from 'src/entities/device.entity';
 import { DeviceService } from './device.service';
+import { DeviceController } from './device.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Device])],
   providers: [DeviceService],
-  controllers: [],
+  controllers: [DeviceController],
   exports: [DeviceService],
 })
 export class DeviceModule {}
