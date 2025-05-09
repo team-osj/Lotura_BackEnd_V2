01e0a5f8db7155313526837af6edb0ae58ea3a44
velyn666 <ni1722650@gmail.com>
2025-05-09 11:02:35 +0900

    널러블

--- src/entities/notice.entity.ts
+++ src/entities/notice.entity.ts
@@ -1,14 +1,21 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+import { IsOptional } from 'class-validator';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
 
 @Entity('Notice')
 export class Notice {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 200 })
+  @Column({ length: 200, nullable: true })
+  @IsOptional()
   title: string;
 
-  @Column({ type: 'mediumtext' })
+  @Column({ type: 'mediumtext', nullable: true })
   contents: string;
 
   @CreateDateColumn()
